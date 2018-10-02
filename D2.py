b15bd3f5d1083d4c8656470c9937960fcfa73c82
Az <z0rk@protonmail.com>
2018-10-02 17:58:01 +0200

    Update D2.py

--- D2.py
+++ D2.py
@@ -38,11 +38,9 @@ if __name__=="__main__":
     threads=sys.argv[3] #raw_input("Enter number of threads:")
     threads=int(threads)
     port=int(port)
-    UsAg=UserAgent()
-
     headers = """ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
     Accept-Language: en-us,en;q=0.5
-    Windows / IE 6: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
+    User-Agent: Windows / IE 6: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
     Accept-Encoding: gzip,deflate
     Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
     Keep-Alive: 115 """
